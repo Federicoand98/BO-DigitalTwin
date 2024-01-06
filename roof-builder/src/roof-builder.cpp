db8c49869cf497278da4eb7160ed0fdc42691620
Alex Gianelli <56556129+Noesh@users.noreply.github.com>
2024-01-06 13:37:30 +0100

    iter on folder

--- roof-builder/src/roof-builder.cpp
+++ roof-builder/src/roof-builder.cpp
@@ -4,42 +4,46 @@
 #include "roof-builder.h"
 #include <fstream>
 #include <string>
+#include <filesystem>
 #include "Readers/ReaderLas.h"
 
 #include <opencv2/opencv.hpp>
 
 int main() {
+	for (const auto& file : std::filesystem::directory_iterator(ASSETS_PATH "las/")) {
+		try {
+			ReaderLas readerLas(file.path().string());
 
-	try {
-		ReaderLas readerLas(ASSETS_PATH "las/32_684000_4930000.las");
+			readerLas.Read();
 
-		readerLas.Read();
+			pdal::PointViewSet* pointViewSet = readerLas.Get();
+			
+			if (!pointViewSet->empty()) {
+				pdal::PointViewPtr pointView = *pointViewSet->begin();
+				for (pdal::PointId id = 0; id < 5; ++id) {
+					double x = pointView->getFieldAs<double>(pdal::Dimension::Id::X, id);
+					double y = pointView->getFieldAs<double>(pdal::Dimension::Id::Y, id);
+					double z = pointView->getFieldAs<double>(pdal::Dimension::Id::Z, id);
 
-		pdal::PointViewSet* pointViewSet = readerLas.Get();
-		
-		if (!pointViewSet->empty()) {
-			pdal::PointViewPtr pointView = *pointViewSet->begin();
-			for (pdal::PointId id = 0; id < 5; ++id) {
-				double x = pointView->getFieldAs<double>(pdal::Dimension::Id::X, id);
-				double y = pointView->getFieldAs<double>(pdal::Dimension::Id::Y, id);
-				double z = pointView->getFieldAs<double>(pdal::Dimension::Id::Z, id);
-
-				std::cout << x << " " << y << " " << z << std::endl;
+					std::cout << x << " " << y << " " << z << std::endl;
+				}
 			}
+
+			readerLas.Flush();
+
+			std::cout << "\nDone." << std::endl;
+			
+		} catch (const pdal::pdal_error &e) {
+			std::cerr << "PDAL read error: " << e.what() << std::endl;
+			return 1;
+		} catch (const std::exception &e) {
+			std::cerr << "Exception: " << e.what() << std::endl;
+		} catch (...) {
+			std::cerr << "An unknown error occurred." << std::endl;
 		}
+	}
 
-		readerLas.Flush();
-
-		std::cout << "\nDone." << std::endl;
-		
-	} catch (const pdal::pdal_error &e) {
-		std::cerr << "PDAL read error: " << e.what() << std::endl;
-		return 1;
-	} catch (const std::exception &e) {
-		std::cerr << "Exception: " << e.what() << std::endl;
-	} catch (...) {
-        std::cerr << "An unknown error occurred." << std::endl;
-    }
+	
 
 	/*
 	
